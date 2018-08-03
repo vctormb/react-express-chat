52b080c18ba0965b3659e38a2fa2997886ca9192
Victor Martins <vctr.mb@gmail.com>
2018-08-02 23:28:04 -0300

    Added generic request to handle with react routes inside the build folder

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const passport = require('passport');
 
 mongoose.connect('mongodb://admin:a123456@ds243501.mlab.com:43501/react-chat-express');
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('db connected')
+	console.log('db connected')
 });
 
 const app = express();
@@ -18,9 +18,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 const server = require('http').Server(app);
@@ -41,26 +41,30 @@ app.use(passport.initialize());
  * SOCKET
  */
 io.on('connection', async (socket) => {
-  require('./sockets/chat/joinedUser')(io, socket);
-  require('./sockets/chat/chatMessage')(io, socket);
-  require('./sockets/chat/disconnect')(io, socket);
-  require('./sockets/chat/privateMessage')(io, socket);
-  require('./sockets/chat/joinPrivateRoom')(io, socket);
+	require('./sockets/chat/joinedUser')(io, socket);
+	require('./sockets/chat/chatMessage')(io, socket);
+	require('./sockets/chat/disconnect')(io, socket);
+	require('./sockets/chat/privateMessage')(io, socket);
+	require('./sockets/chat/joinPrivateRoom')(io, socket);
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // send error
-  res.status(err.status || 500).send(err);
+	// send error
+	res.status(err.status || 500).send(err);
 });
 
 module.exports = { app, server };
