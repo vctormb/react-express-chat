11049f34b31eb96950383e69431c64a7b83ee4a7
Victor Martins <vctr.mb@gmail.com>
2018-08-02 22:37:45 -0300

    Changed port from node and socket to 5000

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -3,7 +3,7 @@
 import io from 'socket.io-client';
 
 // const socket = io('http://10.11.9.108:9000');
-const socket = io('localhost:9000');
+const socket = io('localhost:5000');
 
 socket.on('connect', (e) => {
     socket.on('disconnect', () => {
